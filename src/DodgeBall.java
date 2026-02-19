52ea4e0e3139a0b2b595f5e9031f114510d6f5e6
Joshua Myers <j_myers8@u.pacific.edu>
2026-02-18 16:47:00 -0800

    enemies populated on screen regularly

--- src/DodgeBall.java
+++ src/DodgeBall.java
@@ -1,47 +1,76 @@
-import acm.graphics.*;
-import acm.program.*;
-import java.awt.*;
-import java.awt.event.*;
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
 import java.util.ArrayList;
-import javax.swing.*;
+
+import javax.swing.Timer;
+
+import acm.graphics.*;
+import acm.program.GraphicsProgram;
+import acm.util.RandomGenerator;
 
 public class DodgeBall extends GraphicsProgram implements ActionListener {
-	public static final int PROGRAM_HEIGHT = 600;
-	public static final int PROGRAM_WIDTH = 800;
-	public static final int SIZE = 25;
-	public static final int MS = 50;
-	public static final int SPEED = 2;
-	
 	private ArrayList<GOval> balls;
+	private ArrayList<GRect> enemies;
+	private GLabel text;
 	private Timer movement;
-
-	public void init() {
-		setSize(PROGRAM_WIDTH, PROGRAM_HEIGHT);
-		requestFocus();
-	}
+	private RandomGenerator rgen;
+	private int numTimes = 0; // Tracks survival time/enemy pacing
+	
+	public static final int SIZE = 25;
+	public static final int SPEED = 2;
+	public static final int MS = 50;
+	public static final int MAX_ENEMIES = 10;
+	public static final int WINDOW_HEIGHT = 600;
+	public static final int WINDOW_WIDTH = 300;
 	
 	public void run() {
+		rgen = RandomGenerator.getInstance();
 		balls = new ArrayList<GOval>();
+		enemies = new ArrayList<GRect>();
+		
+		text = new GLabel("0", 0, WINDOW_HEIGHT);
+		add(text);
+		
 		movement = new Timer(MS, this);
 		movement.start();
 		addMouseListeners();
 	}
 	
-	public void mousePressed(MouseEvent e) {
-		// Check if any ball is still near the start
-		for (GOval b : balls) {
-			if (b.getX() < 100) return;
+	public void actionPerformed(ActionEvent e) {
+		numTimes++;
+		moveAllBallsOnce();
+		moveAllEnemiesOnce();
+		
+		// Periodic enemy spawning
+		if (numTimes % 40 == 0) {
+			addAnEnemy();
 		}
 		
-		GOval ball = makeBall(SIZE/2, e.getY());
-		balls.add(ball);
-		add(ball);
+		checkCollisions();
+		
+		// Lose condition
+		if (enemies.size() > MAX_ENEMIES) {
+			movement.stop();
+			removeAll();
+			add(new GLabel("You lost! Survival: " + numTimes, 50, WINDOW_HEIGHT/2));
+		}
 	}
 	
-	public void actionPerformed(ActionEvent e) {
-		for (GOval ball : balls) {
-			ball.move(SPEED, 0);
+	public void mousePressed(MouseEvent e) {
+		for(GOval b:balls) {
+			if(b.getX() < SIZE * 2.5) {
+				return;
+			}
 		}
+		addABall(e.getY());     
+	}
+	
+	private void addABall(double y) {
+		GOval ball = makeBall(SIZE/2, y);
+		add(ball);
+		balls.add(ball);
 	}
 	
 	public GOval makeBall(double x, double y) {
@@ -51,7 +80,31 @@ public class DodgeBall extends GraphicsProgram implements ActionListener {
 		return temp;
 	}
 	
-	public static void main(String[] args) {
-		new BallLauncher().start();
+	private void addAnEnemy() {
+		GRect e = makeEnemy(rgen.nextInt(0, WINDOW_HEIGHT-SIZE/2));
+		enemies.add(e);
+		text.setLabel("" + enemies.size());
+		add(e);
+	}
+	
+	public GRect makeEnemy(double y) {
+		GRect temp = new GRect(WINDOW_WIDTH-SIZE, y-SIZE/2, SIZE, SIZE);
+		temp.setColor(Color.GREEN);
+		temp.setFilled(true);
+		return temp;
+	}
+
+	private void moveAllBallsOnce() {
+		for(GOval ball:balls) {
+			ball.move(SPEED, 0);
+		}
+	}
+	
+	public void init() {
+		setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+	}
+	
+	public static void main(String args[]) {
+		new DodgeBall().start();
 	}
 }
